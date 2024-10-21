30785686e06743bbc8f4eaef49bba50bb606dc22
Yamini Yadav <yaminiy1183@gmail.com>
2024-10-21 21:28:54 +0530

    Day 1

---
+++ Datatypes.js
@@ -0,0 +1,10 @@
+let name = "Yamini"    //string
+let age = 21     //number
+let isMarried = false    //boolean 
+let state;    //undefined
+
+//null => standalone value
+// symbol => unique
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
---
+++ variables.js
@@ -0,0 +1,7 @@
+const studentId = 101 //cannot be changed later
+let studentName = "Yamini" //can be altered 
+var studentContact = 9324355224 // can be altered later but is no longer used as it has no block scope 
+
+console.log(studentId)
+console.table([studentId, studentName, studentContact])
+
