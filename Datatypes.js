37a272f13994c2e2bf166e71839fe27ed7d75721
Yamini Yadav <yaminiy1183@gmail.com>
2024-10-24 12:42:53 +0530

    Day 3

--- Datatypes.js
+++ Datatypes.js
@@ -7,4 +7,35 @@ let state;    //undefined
 // symbol => unique
 
 console.log(typeof undefined); // undefined
+const id = Symbol('123')       //Symbol
+const anotherId = Symbol('123')  //Symbol
+
+console.log(id===anotherId); //false
+
 console.log(typeof null); // object
+
+
+// Primitive 
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+
+// Reference (Non Primitive)
+
+// Array, Objects, Functions
+
+const heros = ["IronMan", "Thor", "Loki", "Hulk"]  //Array
+
+let myObj = {
+    name: "Yamini",
+    age: 21,
+}  //object
+
+const myfunction = function(){
+     console.log("Hello World!");
+     
+}  //function
+
+ console.log(typeof heros); //object
+ console.log(typeof myfunction);  //function or object function
+ console.log(typeof myObj);  //object
